082b1a80951090e56f235c954a0a551a258abf8a
SANTIAGO FIERRO <santimarcelofierrod@gmail.com>
2024-01-27 18:39:10 -0500

    Update dependencies to specific versions

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 pip install django
-pip install keras
-pip install numpy
-pip install tensorflow
-pip install joblib
-pip install scikit-learn
+pip install keras==2.15.0
+pip install numpy==1.24.3
+pip install tensorflow==2.15.0
+pip install joblib==1.2.0
+pip install scikit-learn==1.3.0
 pip install django-cors-headers
